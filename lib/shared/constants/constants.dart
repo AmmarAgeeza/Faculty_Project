c92e68140cdfbd2bbcd200dbcc8364a7b3f50b55
AmmarAgeeza <72443818+AmmarAgeeza@users.noreply.github.com>
2022-08-20 22:34:35 +0200

    Revert "ques"

--- lib/shared/constants/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-import 'package:google_fonts/google_fonts.dart';
-
-import 'package:flutter/material.dart';
-
-final body = GoogleFonts.abel(
-    fontSize: 15, fontWeight: FontWeight.normal, color: Color(0xFF222E45));
-final topic = GoogleFonts.abel(
-    fontSize: 30, fontWeight: FontWeight.bold, color: Color(0xFF222E45));
