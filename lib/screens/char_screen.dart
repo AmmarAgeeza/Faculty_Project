a098d6456af7f8683cd71f54f60aed0524eb8291
AmmarAgeeza <ammarfathy516@gmail.com>
2022-08-23 04:05:27 +0200

    solve conflict

--- lib/screens/char_screen.dart
+++ lib/screens/char_screen.dart
@@ -5,6 +5,6 @@ class ChartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Chart'),);
+    return const Center(child: Text('Chart'));
   }
 }
